881df3a53db25e5e7e829b175a1eb12468e80540
Asmaa Aziz <asmaa.aziz99@gmail.com>
2025-04-09 12:10:46 +0200

    Updates

---
+++ assets/PageNotFound-BanKyYg9.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-CKnDOvr3.js";function t(){return n.jsx("div",{children:n.jsx("h1",{children:"Page not found 😢"})})}export{t as default};
---
+++ assets/Product.module-BngNpGNs.js
@@ -0,0 +1,1 @@
+const t="_product_63f8a_1",o={product:t};export{o as s};
